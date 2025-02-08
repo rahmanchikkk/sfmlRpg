c8412aadf468d56a9574bfd2ea96ace7bfda576a
rahmanchik <nahorniyroman@gmail.com>
2025-02-08 16:39:17 +0200

    db

---
+++ db.sql
@@ -0,0 +1,32 @@
+create table skin (
+    skin_id BIGSERIAL NOT NULL PRIMARY KEY,
+    skin VARCHAR(50) NOT NULL,
+    price NUMERIC(10, 2) CHECK (price > 0)
+);
+
+create table client (
+    client_uid UUID NOT NULL PRIMARY KEY,
+    nickname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL, 
+    pwd VARCHAR(50) NOT NULL,
+    gold NUMERIC(20, 2) NOT NULL,
+    skins integer[],
+    UNIQUE(email),
+    UNIQUE(nickname)
+);
+
+create table message (
+    message_uid UUID NOT NULL PRIMARY KEY,
+    sender UUID REFERENCES client(client_uid),
+    receiver UUID REFERENCES client(client_uid),
+    msg VARCHAR(50) NOT NULL    
+);
+
+
+create table game (
+    game_uid UUID NOT NULL PRIMARY KEY,
+    winner UUID REFERENCES client(client_uid),
+    players UUID[]
+);
+
+insert into skin (skin, price) values ('pes patron', '999.99');
